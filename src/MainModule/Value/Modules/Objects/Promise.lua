e1d9cc2cd3e421018b58a0f91b265405228916ba
Avafe <65048459+ImAvafe@users.noreply.github.com>
2025-09-07 12:33:23 -0400

    Update package references

--- src/MainModule/Value/Modules/Objects/Promise.lua
+++ src/MainModule/Value/Modules/Objects/Promise.lua
@@ -1,6 +1,6 @@
 --!strict
 local main = script:FindFirstAncestor("MainModule")
 local packages = main.Value.Packages
-local package = packages["promise"]
+local package = packages["Promise"]
 local Package = require(package)
 return Package
