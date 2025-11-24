b6a3920abcea55153ce716dc03915d75761ca3be
Miloshi <61902773+MiIoshiee@users.noreply.github.com>
2025-11-25 00:14:45 +0100

    Update getDescription.lua

--- src/MainModule/Value/Modules/PlayerUtil/getDescription.lua
+++ src/MainModule/Value/Modules/PlayerUtil/getDescription.lua
@@ -1,7 +1,13 @@
 --!strict
 local getHumanoid = require(script.Parent.getHumanoid)
+local Players = game:GetService("Players")
 return function(player: Player?): HumanoidDescription?
-	local humanoid = getHumanoid(player)
-	local hd = humanoid and humanoid:GetAppliedDescription()
-	return hd
+	if type(player) == "number" then -- Checks if it's a userId, which if it isn't; it proceeds as normal.
+		local hd = Players:GetHumanoidDescriptionFromUserId(player)
+		return hd
+	else
+		local humanoid = getHumanoid(player)
+		local hd = humanoid and humanoid:GetAppliedDescription()
+		return hd
+	end
 end
